149848503d7b9b1593b20874eafb10b213268ba6
Flávio Bianchetti <fjb.developer@gmail.com>
2022-07-27 14:44:59 -0300

    feat:(study_schedule) Informar número de estudantes estudando no mesmo horário
    Detalhes:
    - Implementada função study_schedule.
    
    Requisito 1 atendido.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    
+    result = None
+    if target_time is not None:
+        result = 0
+        for period in permanence_period:
+            value1 = isinstance(period[0], int)
+            value2 = isinstance(period[1], int)
+            if not value1 or not value2:
+                return None
+            if period[0] <= target_time <= period[1]:
+                result += 1
+    return result
